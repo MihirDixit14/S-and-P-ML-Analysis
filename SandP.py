d04f08f42ef7b013e2d8d6daa8fd6a741f9cd308
MihirDixit14 <mdixit@uwm.edu>
2024-12-11 14:20:43 -0500

    Linear regression

--- SandP.py
+++ SandP.py
@@ -21,7 +21,7 @@ from scipy import stats
 df=pd.read_csv(".\sp500_companies.csv")
 
 #Here we can get the general view of the dataset
-# print(df.head())
+print(df.head())
 print(df.info())
 print(df.columns)
 #check the column name data types. Here is an example of Current price
@@ -31,11 +31,19 @@ print(df.Currentprice.duplicated())
 
 
 # %% [markdown]
+# Basic Operations performed: 
+# Calculate the important values of Mean, median, Mode
+# 1. Mean- The average Value of the current price, 
+# 2. Median- The midpoint value of the current price, 
+# 3. Mode: The most common value of the current price  
+# 4. Standard Deviation: A number that describes how spread out the values of the current price.
+
+# %% [markdown]
 # <!-- # Calculate the important values of Mean, median, Mode
 # 1. Mean- The average Value, 
 # 2. Median- The midpoint value, 
 # 3. Mode: The most common value  
-# 4. Standard Deviation: A number that describes how spread out the values are-->
+# 4. Standard Deviation: A number that describes how spread out the values.-->
 
 # %%
 #Mean of the current price in the S and P dataset
@@ -56,7 +64,25 @@ print('Mode=',Mode)
 Std=df['Currentprice'].std()
 print('Standard Deviation=',Std)
 
+# %% [markdown]
+# Machine Learning Models
+# Linear regression. Lets check if there is a positive relation between Current price and Marketcap.
+
+# %% [markdown]
+# <!-- Linear regression of the  S and P dataset 
+# In order to test out linear regression lets check slope, 
+# -->
+
 # %%
+current_price=df['Currentprice']
+MarketCap=df['Marketcap']
+slope,intercept, r, p, std_err=stats.linregress(current_price,MarketCap)
+print('Slope=',slope)
+print('intercept=',intercept)
+print('r=',r)
+
+
+
 
 
 
---
+++ readme.md
@@ -0,0 +1,20 @@
+Here is my new ML Project based on Stock price of S and P.
+
+#Programming Language: Python
+#IDE: VSCode
+#Dataset: Sand P500.csv (Kaggle.com)
+
+Project Overview
+This project aims to analyze and visualize the stock prices of S&P 500 companies using various machine learning models and data analysis techniques. These techniques will be helpful in understanding the behavior of S&P stock prices and how different factors like industry, currentprice, revenue growth.
+
+Basic Operations performed: 
+Calculate the important values of Mean, median, Mode
+1. Mean- The average Value of the current price, 
+2. Median- The midpoint value of the current price, 
+3. Mode: The most common value of the current price  
+4. Standard Deviation: A number that describes how spread out the values of the current price.
+
+Findings and interpretations
+Machine Learning Models
+Linear regression. Lets check if there is a positive relation between Current price and Marketcap. The low r squared valued 0.0437 indicated that marketcap may not be a strong interpreter of current price.
+
